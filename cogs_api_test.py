1548eadde8b003bac5b9480680f7c8809497c98f
Andy Sayler <andy.sayler@gmail.com>
2014-07-28 13:38:46 -0600

    Moved DB processing to per-test setup/teardown

--- cogs_api_test.py
+++ cogs_api_test.py
@@ -19,31 +19,38 @@ class CogsApiTestCase(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cogs_api.datatypes._REDIS_DB += _REDIS_TESTDB_OFFSET
-        db = redis.StrictRedis(host=cogs_api.datatypes._REDIS_HOST,
-                               port=cogs_api.datatypes._REDIS_PORT,
-                               db=cogs_api.datatypes._REDIS_DB)
-        if (db.dbsize() != 0):
-            raise DatatypesTestError("Test Database Not Empty: {}".format(db.dbsize()))
+        pass
 
     @classmethod
     def tearDownClass(cls):
-        db = redis.StrictRedis(host=cogs_api.datatypes._REDIS_HOST,
-                               port=cogs_api.datatypes._REDIS_PORT,
-                               db=cogs_api.datatypes._REDIS_DB)
-        db.flushdb()
-        cogs_api.datatypes._REDIS_DB -= _REDIS_TESTDB_OFFSET
+        pass
 
     def setUp(self):
+        cogs_api.datatypes._REDIS_DB += _REDIS_TESTDB_OFFSET
+        self.db = redis.StrictRedis(host=cogs_api.datatypes._REDIS_HOST,
+                                    port=cogs_api.datatypes._REDIS_PORT,
+                                    db=cogs_api.datatypes._REDIS_DB)
+        if (self.db.dbsize() != 0):
+            raise Exception("Test Database Not Empty: {}".format(db.dbsize()))
         self.app = cogs_api.app.test_client()
 
     def tearDown(self):
-        pass
+        self.db.flushdb()
 
     def test_root_get(self):
         res = self.app.get('/')
         self.assertEqual(res.status_code, 200, "Bad return status")
         self.assertEqual(res.data, cogs_api._MSG_ROOT, "Data not eqqal")
+
+    def test_create_assignment(self):
+        d = {'name': "Test_Assignment_Create",
+             'contact': "Andy Sayler"}
+        ds = json.dumps(d)
+        res = self.app.post('/assignments/', data=ds)
+        out = json.loads(res.data)
+        out_uuid = uuid.UUID(out.keys()[0])
+        out_d = out[str(out_uuid)]
+        self.assertEqual(d, out_d, "Return does not match input")
 
     def test_create_assignment(self):
         d = {'name': "Test_Assignment_Create",
