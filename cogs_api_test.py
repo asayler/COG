6f8c73db3fa2e0589c33f04e6297cb3eaab1ca71
Andy Sayler <andy.sayler@gmail.com>
2014-07-28 13:46:23 -0600

    Added get assignment API test

--- cogs_api_test.py
+++ cogs_api_test.py
@@ -52,15 +52,18 @@ class CogsApiTestCase(unittest.TestCase):
         out_d = out[str(out_uuid)]
         self.assertEqual(d, out_d, "Return does not match input")
 
-    def test_create_assignment(self):
-        d = {'name': "Test_Assignment_Create",
+    def test_get_assignment(self):
+        d = {'name': "Test_Assignment_Get",
              'contact': "Andy Sayler"}
         ds = json.dumps(d)
-        res = self.app.post('/assignments/', data=ds)
-        out = json.loads(res.data)
-        out_uuid = uuid.UUID(out.keys()[0])
-        out_d = out[str(out_uuid)]
-        self.assertEqual(d, out_d, "Return does not match input")
+        res_a = self.app.post('/assignments/', data=ds)
+        a_dict = json.loads(res_a.data)
+        a_uuid = uuid.UUID(a_dict.keys()[0])
+        res_b = self.app.get('/assignments/{:s}/'.format(a_uuid))
+        b_dict = json.loads(res_b.data)
+        b_uuid = uuid.UUID(b_dict.keys()[0])
+        self.assertEqual(a_uuid, b_uuid, "UUIDs do not match")
+        self.assertEqual(a_dict[str(a_uuid)], b_dict[str(b_uuid)], "Attributes do not match")
 
 
 ### Main
