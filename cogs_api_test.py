31f912a2430631e019b78ec5e5804c2e4c32c68c
Andy Sayler <andy.sayler@gmail.com>
2014-07-28 21:11:11 -0600

    Refactored API tests to split into multible classes and use static dicts

--- cogs_api_test.py
+++ cogs_api_test.py
@@ -5,8 +5,9 @@
 # Summer 2014
 # Univerity of Colorado
 
-import unittest
+import copy
 import json
+import unittest
 import uuid
 
 import redis
@@ -16,6 +17,14 @@ import cogs_api
 _REDIS_TESTDB_OFFSET = 1
 
 _ASSIGNMENTS_KEY = "assignments"
+_ASSIGNMENTTESTS_KEY = "tests"
+
+_ASSIGNMENT_TESTDICT      = {'name': "Test_Assignment",
+                             'contact': "Andy Sayler"}
+_ASSIGNMENTTEST_TESTDICT  = {'name': "Test_Assignment",
+                             'contact': "Andy Sayler",
+                             'path': "/tmp/test.py",
+                             'maxscore': "10"}
 
 class CogsApiTestCase(unittest.TestCase):
 
@@ -33,109 +42,141 @@ class CogsApiTestCase(unittest.TestCase):
                                     port=cogs_api.datatypes._REDIS_PORT,
                                     db=cogs_api.datatypes._REDIS_DB)
         if (self.db.dbsize() != 0):
-            raise Exception("Test Database Not Empty: {}".format(db.dbsize()))
+            raise Exception("Test Database Not Empty: {}".format(self.db.dbsize()))
         self.app = cogs_api.app.test_client()
 
     def tearDown(self):
         self.db.flushdb()
         cogs_api.datatypes._REDIS_DB -= _REDIS_TESTDB_OFFSET
 
+
+## Root Tests
+class CogsApiRootTestCase(CogsApiTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def setUp(self):
+        super(CogsApiRootTestCase, self).setUp()
+
+    def tearDown(self):
+        super(CogsApiRootTestCase, self).tearDown()
+
     def test_root_get(self):
         res = self.app.get('/')
-        self.assertEqual(res.status_code, 200, "Bad return status")
-        self.assertEqual(res.data, cogs_api._MSG_ROOT, "Data not eqqal")
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(res.data, cogs_api._MSG_ROOT)
+
+
+## Assignment Tests
+class CogsApiAssignmentsTestCase(CogsApiTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def setUp(self):
+        super(CogsApiAssignmentsTestCase, self).setUp()
+
+    def tearDown(self):
+        super(CogsApiAssignmentsTestCase, self).tearDown()
 
     def test_create_assignment(self):
 
         # Create Assignment
-        d = {'name': "Test_Assignment_Create",
-             'contact': "Andy Sayler"}
-        ds = json.dumps(d)
+        ds = json.dumps(_ASSIGNMENT_TESTDICT)
         res = self.app.post('/assignments/', data=ds)
-        self.assertEqual(res.status_code, 200, "Bad return status")
-        out = json.loads(res.data)
-        out_uuid = uuid.UUID(out.keys()[0])
-        out_d = out[str(out_uuid)]
-        self.assertEqual(d, out_d, "Attributes do not match")
+        self.assertEqual(res.status_code, 200)
+
+        # Check Assignment
+        asn = json.loads(res.data)
+        asn_uuid = uuid.UUID(asn.keys()[0])
+        asn_d = asn[str(asn_uuid)]
+        self.assertEqual(_ASSIGNMENT_TESTDICT, asn_d)
 
     def test_get_assignment(self):
 
         # Create Assignment
-        d = {'name': "Test_Assignment_Get",
-             'contact': "Andy Sayler"}
-        ds = json.dumps(d)
-        res_a = self.app.post('/assignments/', data=ds)
-        self.assertEqual(res_a.status_code, 200, "Bad return status")
-        a_dict = json.loads(res_a.data)
-        a_uuid = uuid.UUID(a_dict.keys()[0])
-        self.assertEqual(d, a_dict[str(a_uuid)], "Attributes do not match")
+        ds = json.dumps(_ASSIGNMENT_TESTDICT)
+        res = self.app.post('/assignments/', data=ds)
+        self.assertEqual(res.status_code, 200)
+        asn_in = json.loads(res.data)
+        asn_in_uuid = uuid.UUID(asn_in.keys()[0])
 
         # Get Assignment
-        res_b = self.app.get('/assignments/{:s}/'.format(a_uuid))
-        self.assertEqual(res_b.status_code, 200, "Bad return status")
-        b_dict = json.loads(res_b.data)
-        b_uuid = uuid.UUID(b_dict.keys()[0])
-        self.assertEqual(a_uuid, b_uuid, "UUIDs do not match")
-        self.assertEqual(a_dict[str(a_uuid)], b_dict[str(b_uuid)], "Attributes do not match")
+        res = self.app.get('/assignments/{:s}/'.format(asn_in_uuid))
+        self.assertEqual(res.status_code, 200)
+        asn_out = json.loads(res.data)
+        asn_out_uuid = uuid.UUID(asn_out.keys()[0])
+        self.assertEqual(asn_in_uuid, asn_out_uuid)
+        self.assertEqual(asn_in[str(asn_in_uuid)], asn_out[str(asn_out_uuid)])
 
     def test_set_assignment(self):
 
         # Create Assignment
-        d_a = {'name': "Test_Assignment_Set_A",
-               'contact': "Andy Sayler"}
+        d_a = copy.deepcopy(_ASSIGNMENT_TESTDICT)
         ds_a = json.dumps(d_a)
         res_a = self.app.post('/assignments/', data=ds_a)
-        self.assertEqual(res_a.status_code, 200, "Bad return status")
+        self.assertEqual(res_a.status_code, 200)
         a_dict = json.loads(res_a.data)
         a_uuid = uuid.UUID(a_dict.keys()[0])
-        self.assertEqual(d_a, a_dict[str(a_uuid)], "Attributes do not match")
 
         # Update Assignment
-        d_b = {'name': "Test_Assignment_Set_B",
-               'contact': "Andy Sayler III"}
+        d_b = copy.deepcopy(_ASSIGNMENT_TESTDICT)
+        for k in d_b:
+            d_b[k] = d_b[k] + "_update"
+        self.assertNotEqual(d_a, d_b)
         ds_b = json.dumps(d_b)
         res_b = self.app.put('/assignments/{:s}/'.format(a_uuid), data=ds_b)
-        self.assertEqual(res_b.status_code, 200, "Bad return status")
+        self.assertEqual(res_b.status_code, 200)
         b_dict = json.loads(res_b.data)
         b_uuid = uuid.UUID(b_dict.keys()[0])
-        self.assertEqual(a_uuid, b_uuid, "UUIDs do not match")
-        self.assertEqual(d_b, b_dict[str(b_uuid)], "Return does not match input")
+        self.assertEqual(a_uuid, b_uuid)
+        self.assertEqual(d_b, b_dict[str(b_uuid)])
 
         # Get Assignment
         res_c = self.app.get('/assignments/{:s}/'.format(a_uuid))
-        self.assertEqual(res_c.status_code, 200, "Bad return status")
+        self.assertEqual(res_c.status_code, 200)
         c_dict = json.loads(res_c.data)
         c_uuid = uuid.UUID(c_dict.keys()[0])
-        self.assertEqual(b_uuid, c_uuid, "UUIDs do not match")
-        self.assertEqual(b_dict[str(b_uuid)], c_dict[str(c_uuid)], "Attributes do not match")
+        self.assertEqual(b_uuid, c_uuid)
+        self.assertEqual(b_dict[str(b_uuid)], c_dict[str(c_uuid)])
 
     def test_delete_assignment(self):
 
         # Create Assignment
-        d_a = {'name': "Test_Assignment_Delete",
-               'contact': "Andy Sayler"}
+        d_a = copy.deepcopy(_ASSIGNMENT_TESTDICT)
         ds_a = json.dumps(d_a)
         res_a = self.app.post('/assignments/', data=ds_a)
-        self.assertEqual(res_a.status_code, 200, "Bad return status")
+        self.assertEqual(res_a.status_code, 200)
         a_dict = json.loads(res_a.data)
         a_uuid = uuid.UUID(a_dict.keys()[0])
-        self.assertEqual(d_a, a_dict[str(a_uuid)], "Attributes do not match")
+        self.assertEqual(d_a, a_dict[str(a_uuid)])
 
         # Get Assignment
         res_b = self.app.get('/assignments/{:s}/'.format(a_uuid))
-        self.assertEqual(res_b.status_code, 200, "Bad return status")
+        self.assertEqual(res_b.status_code, 200)
         b_dict = json.loads(res_b.data)
         b_uuid = uuid.UUID(b_dict.keys()[0])
-        self.assertEqual(a_uuid, b_uuid, "UUIDs do not match")
-        self.assertEqual(a_dict[str(a_uuid)], b_dict[str(b_uuid)], "Attributes do not match")
+        self.assertEqual(a_uuid, b_uuid)
+        self.assertEqual(a_dict[str(a_uuid)], b_dict[str(b_uuid)])
 
         # Delete Assignment
         res_c = self.app.delete('/assignments/{:s}/'.format(a_uuid))
-        self.assertEqual(res_c.status_code, 200, "Bad return status")
+        self.assertEqual(res_c.status_code, 200)
 
         # Get Assignment
         res_b = self.app.get('/assignments/{:s}/'.format(a_uuid))
-        self.assertEqual(res_b.status_code, 404, "Bad return status")
+        self.assertEqual(res_b.status_code, 404)
 
     def test_list_assignments(self):
 
@@ -143,10 +184,10 @@ class CogsApiTestCase(unittest.TestCase):
 
         # Get Assignments (Empty)
         res_a = self.app.get('/assignments/')
-        self.assertEqual(res_a.status_code, 200, "Bad return status")
+        self.assertEqual(res_a.status_code, 200)
         a_dict = json.loads(res_a.data)
         assignments_out = set(a_dict[_ASSIGNMENTS_KEY])
-        self.assertEqual(assignments_in, assignments_out, "Assignment lists do not match")
+        self.assertEqual(assignments_in, assignments_out)
 
         # Create Assignments
         for i in range(10):
