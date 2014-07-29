f13fc98cde7569afacaea6b8aaa485a218762fc9
Andy Sayler <andy.sayler@gmail.com>
2014-07-28 21:10:21 -0600

    Added test endpoints

--- cogs_api.py
+++ cogs_api.py
@@ -15,13 +15,17 @@ _MSG_ROOT = "Welcome to the CU CS Online Grading System API"
 
 ### Endpoints
 
+## Root Endpoints
+
 @app.route("/", methods=['GET'])
 def get_root():
     res = _MSG_ROOT
     return res
 
+## Assignment Endpoints
+
 @app.route("/assignments/", methods=['GET'])
-def get_assignments():
+def list_assignments():
     s = datatypes.Server()
     assignments = s.list_assignments()
     out = {'assignments': list(assignments)}
@@ -124,6 +128,104 @@ def delete_assignment(uuid_hex):
     out = {}
     res = flask.jsonify(out)
     return res
+
+## Assignment Test Endpoints
+
+@app.route("/assignments/<asn_uuid>/tests/", methods=['GET'])
+def list_assignment_tests(asn_uuid):
+
+    # Get Assignment
+    try:
+        asn = datatypes.Assignment.from_existing(asn_uuid)
+    except KeyError as e:
+        err = {
+            'status': 404,
+            'message': str(e)
+        }
+        err_res = flask.jsonify(err)
+        err_res.status_code = err['status']
+        return err_res
+
+    # List Assignment Tests
+    tests = asn.list_tests()
+
+    # Return Assignment Test
+    out = {'tests': list(tests)}
+    res = flask.jsonify(out)
+    return res
+
+@app.route("/assignments/<asn_uuid>/tests/", methods=['POST'])
+def create_assignment_test(asn_uuid):
+
+    # Get Assignment
+    try:
+        asn = datatypes.Assignment.from_existing(asn_uuid)
+    except KeyError as e:
+        err = {
+            'status': 404,
+            'message': str(e)
+        }
+        err_res = flask.jsonify(err)
+        err_res.status_code = err['status']
+        return err_res
+
+    # Creat Assignment Test
+    d = flask.request.get_json(force=True)
+    try:
+        tst = asn.create_test(d)
+    except KeyError as e:
+        err = {
+            'status': 400,
+            'message': str(e)
+        }
+        err_res = flask.jsonify(err)
+        err_res.status_code = err['status']
+        return err_res
+
+    # Return Assignment Test
+    out = {repr(tst): tst.get_dict()}
+    res = flask.jsonify(out)
+    return res
+
+@app.route("/assignments/<asn_uuid>/tests/<tst_uuid>/", methods=['GET'])
+def get_assignment_test(asn_uuid, tst_uuid):
+
+    # Get Assignment
+    try:
+        asn = datatypes.Assignment.from_existing(asn_uuid)
+    except KeyError as e:
+        err = {
+            'status': 404,
+            'message': str(e)
+        }
+        err_res = flask.jsonify(err)
+        err_res.status_code = err['status']
+        return err_res
+
+    # Get Assignment Test
+    try:
+        tst = asn.get_test(tst_uuid)
+    except KeyError as e:
+        err = {
+            'status': 404,
+            'message': str(e)
+        }
+        err_res = flask.jsonify(err)
+        err_res.status_code = err['status']
+        return err_res
+
+    # Return Assignment Test
+    out = {repr(tst): tst.get_dict()}
+    res = flask.jsonify(out)
+    return res
+
+## Other Endpoints
+
+@app.route("/test/", methods=['POST'])
+def test_upload():
+    print("Testing upload...")
+    print("files = {:s}".format(flask.request.files))
+    return flask.jsonify({"status": "done"})
 
 ### Exceptions
 
