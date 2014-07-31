758a84e0ae2a1c157b2bfe8d48418a4c7579cb3d
Andy Sayler <andy.sayler@gmail.com>
2014-07-31 10:44:30 -0600

    Moved to static dicationary file tracking

--- cogs/environment.py
+++ cogs/environment.py
@@ -31,19 +31,28 @@ class Env(object):
             self.tst_files.append(self.copy_to_wd(fle))
 
     def copy_to_wd(self, fle):
+        new_fle = fle.get_dict()
+        src = os.path.abspath("{:s}".format(new_fle['path']))
         dst = os.path.abspath("{:s}/{:s}".format(self.wd, fle['name']))
-        new_fle = fle.copy(dst)
+        shutil.copy(src, dst)
+        new_fle['path'] = dst
         return new_fle
 
     def close(self):
 
         # Delete Submission Files
         for fle in self.sub_files:
-            fle.delete(force=True)
+            try:
+                os.remove(fle['path'])
+            except OSError:
+                pass
 
         # Delete Tester Files
         for fle in self.tst_files:
-            fle.delete(force=True)
+            try:
+                os.remove(fle['path'])
+            except OSError:
+                pass
 
         # Delete Directory
         shutil.rmtree(self.wd)
