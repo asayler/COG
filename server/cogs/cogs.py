40e02e5168509ebb9335b727e9d94a83806a2abb
Andy Sayler <andy.sayler@gmail.com>
2014-08-05 18:39:13 -0600

    Transtions cogs types and tests

--- server/cogs/cogs.py
+++ server/cogs/cogs.py
@@ -4,25 +4,28 @@
 # Summer 2014
 # Univerity of Colorado
 
+
 import backend_redis as backend
+from backend_redis import BackendError, FactoryError, ObjectError, ObjectDNE
 
 
-_BASE_SCHEMA = ['created_time', 'modified_time']
+_TS_SCHEMA = ['created_time', 'modified_time']
 _USER_SCHEMA = ['username', 'first', 'last', 'type']
 _GROUP_SCHEMA = ['name', 'members']
 _COL_PERMISSION_SCHEMA = ['create', 'list']
 _OBJ_PERMISSION_SCHEMA = ['read', 'write', 'delete']
-_ASSIGNMENTS_SCHEMA = ['name', 'contact', 'permissions']
-_TESTS_SCHEMA = ['name', 'contact', 'type', 'maxscore']
-_SUBMISSIONS_SCHEMA = ['author']
-_RUNS_SCHEMA = ['test', 'status', 'score', 'output']
-_FILES_SCHEMA = ['key', 'name', 'type', 'encoding', 'path']
+_ASSIGNMENT_SCHEMA = ['name', 'contact', 'permissions']
+_TEST_SCHEMA = ['name', 'contact', 'type', 'maxscore']
+_SUBMISSION_SCHEMA = ['author']
+_RUN_SCHEMA = ['test', 'status', 'score', 'output']
+_FILE_SCHEMA = ['key', 'name', 'type', 'encoding', 'path']
 
 _FILES_DIR = "./files/"
 
 
 ### COGS Core Objects ###
 
+## Top-Level Server Object ##
 class Server(object):
     """
     COGS Server Class
@@ -62,3 +65,276 @@ class Server(object):
 
 
 ### COGS Base Objects ###
+
+## User Account Object ##
+class UserBase(backend.TSHashBase):
+    """
+    COGS User Class
+
+    """
+
+    schema = set(_TS_SCHEMA + _USER_SCHEMA)
+
+
+## Assignment Object ##
+class AssignmentBase(backend.TSHashBase):
+    """
+    COGS Assignment Class
+
+    """
+
+    schema = set(_TS_SCHEMA + _ASSIGNMENT_SCHEMA)
+
+    # Override Constructor
+    def __init__(self, uuid_obj):
+        """Base Constructor"""
+
+        # Call Parent Construtor
+        super(AssignmentBase, self).__init__(uuid_obj)
+
+        # Setup Factories
+        self.TestFactory = backend.UUIDFactory(TestBase, prefix=self.full_key, db=self.db)
+        self.SubmissionFactory = backend.UUIDFactory(SubmissionBase, prefix=self.full_key, db=self.db, )
+
+    # Override Delete
+    def delete(self):
+        """Delete"""
+
+        # Delete Tests
+        for t_uuid in self.list_tests():
+            tst = self.get_test(t_uuid)
+            tst.delete()
+
+        # Delete Submissions
+        for s_uuid in self.list_submissions():
+            sub = self.get_submission(s_uuid)
+            sub.delete()
+
+        # Delete Self
+        super(AssignmentBase, self).delete()
+
+    # Test Methods
+    def create_test(self, d):
+        return self.TestFactory.from_new(d)
+    def get_test(self, uuid_hex):
+        return self.TestFactory.from_existing(uuid_hex)
+    def get_tests(self):
+        return self.TestFactory.get_siblings()
+    def list_tests(self):
+        return self.TestFactory.list_siblings()
+
+    # Submission Methods
+    def create_submission(self, d):
+        return self.SubmissionFactory.from_new(d)
+    def get_submission(self, uuid_hex):
+        return self.SubmissionFactory.from_existing(uuid_hex)
+    def get_submissions(self):
+        return self.SubmissionFactory.get_siblings()
+    def list_submissions(self):
+        return self.SubmissionFactory.list_siblings()
+
+
+## Assignment Test Object ##
+class TestBase(backend.TSHashBase):
+    """
+    COGS Test Class
+
+    """
+
+    schema = set(_TS_SCHEMA + _TEST_SCHEMA)
+
+    # Override Constructor
+    def __init__(self, uuid_obj):
+        """Base Constructor"""
+        super(TestBase, self).__init__(uuid_obj)
+        self.FileFactory = backend.UUIDFactory(FileBase, prefix=self.full_key, db=self.db)
+
+    # Override Delete
+    def delete(self):
+        """Delete"""
+
+        # Delete Files
+        for f_uuid in self.list_files():
+            fle = self.get_file(f_uuid)
+            fle.delete(force=True)
+
+        # Delete Self
+        super(TestBase, self).delete()
+
+    # File Methods
+    def create_file(self, d, file_obj):
+        return self.FileFactory.from_new(d, file_obj)
+    def get_file(self, uuid_hex):
+        return self.FileFactory.from_existing(uuid_hex)
+    def get_files(self):
+        return self.FileFactory.get_objs()
+    def list_files(self):
+        return self.FileFactory.list_objs()
+
+
+## Assignment Submission Object ##
+class SubmissionBase(backend.TSHashBase):
+    """
+    COGS Submission Class
+
+    """
+
+    schema = set(_TS_SCHEMA + _SUBMISSION_SCHEMA)
+
+    # Override Constructor
+    def __init__(self, uuid_obj):
+        """Base Constructor"""
+        super(SubmissionBase, self).__init__(uuid_obj)
+        self.FileFactory = backend.UUIDFactory(FileBase, prefix=self.full_key, db=self.db)
+        self.RunFactory = backend.UUIDFactory(RunBase, prefix=self.full_key, db=self.db)
+
+    # Override Delete
+    def delete(self):
+        """Delete"""
+
+        # Delete Files
+        for f_uuid in self.list_files():
+            fle = self.get_file(f_uuid)
+            fle.delete(force=True)
+
+        # Delete Runs
+        for r_uuid in self.list_runs():
+            run = self.get_run(r_uuid)
+            run.delete()
+
+        # Delete Self
+        super(Submission, self).delete()
+
+    # File Methods
+    def create_file(self, d, file_obj):
+        return self.FileFactory.from_new(d, file_obj)
+    def get_file(self, uuid_hex):
+        return self.FileFactory.from_existing(uuid_hex)
+    def get_files(self):
+        return self.FileFactory.get_objs()
+    def list_files(self):
+        return self.FileFactory.list_objs()
+
+    # Run Methods
+    def execute_run(self, tst, sub):
+        return self.RunFactory.from_new(tst, sub)
+    def get_run(self, uuid_hex):
+        return self.RunFactory.from_existing(uuid_hex)
+    def get_runs(self):
+        return self.RunFactory.get_objs()
+    def list_runs(self):
+        return self.RunFactory.list_objs()
+
+
+## Test Run Object ##
+class RunBase(backend.TSHashBase):
+    """
+    COGS Run Class
+
+    """
+
+    schema = set(_TS_SCHEMA + _RUN_SCHEMA)
+
+    # Override from_new
+    @classmethod
+    def from_new(cls, tst, sub):
+        """New Constructor"""
+
+        # Create New Object
+        data = {}
+
+        # Setup Dict
+        data['test'] = repr(tst)
+        data['status'] = ""
+        data['score'] = ""
+        data['output'] = ""
+
+        # Create Run
+        run = super(RunBase, cls).from_new(data)
+
+        # Get Files
+        tst_fls = tst.get_files()
+        sub_fls = sub.get_files()
+
+        # Grade Run
+        env = environment.Env(run, tst_fls, sub_fls)
+        tester = tester_script.Tester(env)
+        ret, score, output = tester.test()
+        env.close()
+
+        # Set Results
+        run['status'] = str(ret)
+        run['score'] = str(score)
+        run['output'] = str(output)
+
+        # Return Run
+        return run
+
+
+## File Object ##
+class FileBase(backend.TSHashBase):
+    """
+    COGS File Class
+
+    """
+
+    schema = set(_TS_SCHEMA + _FILE_SCHEMA)
+
+    # Override from_new
+    @classmethod
+    def from_new(cls, d, file_obj=None, dst=None):
+        """New Constructor"""
+
+        # Create New Object
+        data = copy.deepcopy(d)
+
+        # Setup file_obj
+        if file_obj is None:
+            src_path = os.path.abspath("{:s}".format(data['path']))
+            src_file = open(src_path, 'rb')
+            file_obj = werkzeug.datastructures.FileStorage(stream=src_file, filename=data['name'])
+        else:
+            data['name'] = str(file_obj.filename)
+            data['path'] = ""
+
+        # Get Type
+        typ = mimetypes.guess_type(file_obj.filename)
+        data['type'] = str(typ[0])
+        data['encoding'] = str(typ[1])
+
+        # Create File
+        fle = super(FileBase, cls).from_new(data)
+
+        # Set Path
+        if dst is None:
+            fle['path'] = os.path.abspath("{:s}/{:s}".format(_FILES_DIR, repr(fle)))
+        else:
+            fle['path'] = os.path.abspath("{:s}".format(dst))
+
+        # Save File
+        try:
+            file_obj.save(fle['path'])
+            file_obj.close()
+        except IOError:
+            # Clean up on failure
+            fle.delete(force=True)
+            raise
+
+        # Return File
+        return fle
+
+    # Override Delete
+    def delete(self, force=False):
+        """Delete Object"""
+
+        # Delete File
+        try:
+            os.remove(self['path'])
+        except OSError:
+            if force:
+                pass
+            else:
+                raise
+
+        # Delete Self
+        super(FileBase, self).delete()
---
+++ server/cogs/cogs_test.py
@@ -0,0 +1,154 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Andy Sayler
+# Summer 2014
+# Univerity of Colorado
+
+import copy
+import unittest
+
+import cogs
+import test_common
+
+
+class TypesTestCase(test_common.CogsTestCase):
+
+    def setUp(self):
+        super(TypesTestCase, self).setUp()
+
+    def tearDown(self):
+        super(TypesTestCase, self).tearDown()
+
+
+class ServerTestCase(TypesTestCase):
+
+    def setUp(self):
+        super(ServerTestCase, self).setUp()
+        self.s = cogs.Server(db=self.db)
+
+    def tearDown(self):
+        del(self.s)
+        super(ServerTestCase, self).tearDown()
+
+    def test_list_assignments(self):
+
+        assignments_in = set([])
+
+        # List Assignments (Empty DB)
+        assignments_out = self.s.list_assignments()
+        self.assertEqual(assignments_in, assignments_out)
+
+        # Generate 10 Assingments
+        for i in range(10):
+            d = copy.deepcopy(test_common.ASSIGNMENT_TESTDICT)
+            for k in d:
+                d[k] = "{:s}_test_{:02d}".format(d[k], i)
+            assignments_in.add(self.s.create_assignment(d).obj_key)
+
+        # List Assignments
+        assignments_out = self.s.list_assignments()
+        self.assertEqual(assignments_in, assignments_out)
+
+
+class AssignmentTestCase(TypesTestCase):
+
+    def setUp(self):
+        super(AssignmentTestCase, self).setUp()
+        self.srv = cogs.Server(self.db)
+
+    def tearDown(self):
+        super(AssignmentTestCase, self).tearDown()
+
+    def test_create_assignment(self):
+
+        # Test Empty Dict
+        d = {}
+        self.assertRaises(KeyError, self.srv.create_assignment, d)
+
+        # Test Bad Dict
+        d = {'test': "test"}
+        self.assertRaises(KeyError, self.srv.create_assignment, d)
+
+        # Test Sub Dict
+        d = copy.deepcopy(test_common.ASSIGNMENT_TESTDICT)
+        d.pop(d.keys()[0])
+        self.assertRaises(KeyError, self.srv.create_assignment, d)
+
+        # Test Super Dict
+        d = copy.deepcopy(test_common.ASSIGNMENT_TESTDICT)
+        d['test'] = "test"
+        self.assertRaises(KeyError, self.srv.create_assignment, d)
+
+        # Test Valid
+        d = copy.deepcopy(test_common.ASSIGNMENT_TESTDICT)
+        asn = self.srv.create_assignment(d)
+        self.assertSubset(d, asn.get_dict())
+
+    def test_get_assignment(self):
+
+        # Test Invalid UUID
+        self.assertRaises(cogs.ObjectDNE,
+                          self.srv.get_assignment,
+                          'eb424026-6f54-4ef8-a4d0-bb658a1fc6cf')
+
+        # Test Valid UUID
+        d = copy.deepcopy(test_common.ASSIGNMENT_TESTDICT)
+        asn1 = self.srv.create_assignment(d)
+        asn1_uuid = asn1.obj_key
+        asn2 = self.srv.get_assignment(asn1_uuid)
+        self.assertEqual(asn1, asn2)
+
+class TestTestCase(TypesTestCase):
+
+    def setUp(self):
+        super(TestTestCase, self).setUp()
+        self.srv = cogs.Server(self.db)
+        self.asn = self.srv.create_assignment(test_common.ASSIGNMENT_TESTDICT)
+
+    def tearDown(self):
+        super(TestTestCase, self).tearDown()
+
+    def test_create_test(self):
+
+        # Test Empty Dict
+        d = {}
+        self.assertRaises(KeyError, self.asn.create_test, d)
+
+        # Test Bad Dict
+        d = {'test': "test"}
+        self.assertRaises(KeyError, self.asn.create_test, d)
+
+        # Test Sub Dict
+        d = copy.deepcopy(test_common.TEST_TESTDICT)
+        d.pop(d.keys()[0])
+        self.assertRaises(KeyError, self.asn.create_test, d)
+
+        # Test Super Dict
+        d = copy.deepcopy(test_common.TEST_TESTDICT)
+        d['test'] = "test"
+        self.assertRaises(KeyError, self.asn.create_test, d)
+
+        # Test Valid
+        d = copy.deepcopy(test_common.TEST_TESTDICT)
+        tst = self.asn.create_test(d)
+        self.assertSubset(d, tst.get_dict())
+
+    def test_get_test(self):
+
+        # Test Invalid UUID
+        self.assertRaises(cogs.ObjectDNE,
+                          self.asn.get_test,
+                          'eb424026-6f54-4ef8-a4d0-bb658a1fc6cf')
+
+        # Test Valid UUID
+        d = copy.deepcopy(test_common.TEST_TESTDICT)
+        tst1 = self.asn.create_test(d)
+        tst1_uuid = tst1.obj_key
+        tst2 = self.asn.get_test(tst1_uuid)
+        self.assertEqual(tst1, tst2)
+
+
+# Main
+if __name__ == '__main__':
+    unittest.main()
