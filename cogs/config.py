864c2c8da919b9fa83e1648a9a86777b4abfeb31
Andy Sayler <andy.sayler@gmail.com>
2014-08-22 22:03:57 +0000

    Added config file support for Moodle repmod and authmod

--- cogs/config.py
+++ cogs/config.py
@@ -14,6 +14,10 @@ SEC_REDIS = "redis"
 config.add_section(SEC_REDIS)
 SEC_FILESTORAGE = "filestorage"
 config.add_section(SEC_FILESTORAGE)
+SEC_REPMOD_MOODLE = "repmod_moodle"
+config.add_section(SEC_REPMOD_MOODLE)
+SEC_AUTHMOD_MOODLE = "authmod_moodle"
+config.add_section(SEC_AUTHMOD_MOODLE)
 
 # Set Paths
 MOD_PATH = os.path.dirname(os.path.realpath(__file__))
@@ -26,17 +30,17 @@ config.set(SEC_REDIS, 'PORT', "6379")
 config.set(SEC_REDIS, 'DB', "4")
 config.set(SEC_REDIS, 'PASSWORD', None)
 config.set(SEC_FILESTORAGE, 'PATH', "{:s}/files".format(ROOT_PATH))
+config.set(SEC_AUTHMOD_MOODLE, 'HOST', None)
+config.set(SEC_AUTHMOD_MOODLE, 'SERVICE', None)
+config.set(SEC_REPMOD_MOODLE, 'HOST', None)
+config.set(SEC_REPMOD_MOODLE, 'SERVICE', None)
+config.set(SEC_REPMOD_MOODLE, 'USERNAME', None)
+config.set(SEC_REPMOD_MOODLE, 'PASSWORD', None)
 DEFAULT_SCRIPT_PATH = os.path.realpath("{:s}/scripts".format(ROOT_PATH))
 DEFAULT_ENV_LOCAL_TMP_PATH = "/tmp/cogs"
 DEFAULT_ENV_LOCAL_SANDBOX = "local_sandbox.py"
 DEFAULT_TESTER_SCRIPT_USER = "nobody"
 DEFAULT_TESTER_SCRIPT_GROUP = "nogroup"
-DEFAULT_AUTHMOD_MOODLE_HOST = None
-DEFAULT_AUTHMOD_MOODLE_SERVICE = None
-DEFAULT_REPMOD_MOODLE_HOST = None
-DEFAULT_REPMOD_MOODLE_SERVICE = None
-DEFAULT_REPMOD_MOODLE_USERNAME = None
-DEFAULT_REPMOD_MOODLE_PASSWORD = None
 
 # Read Config File
 config.read(CONF_PATH)
@@ -46,16 +50,23 @@ REDIS_HOST = os.environ.get('COGS_REDIS_HOST', config.get(SEC_REDIS, 'HOST'))
 REDIS_PORT = int(os.environ.get('COGS_REDIS_PORT', config.get(SEC_REDIS, 'PORT')))
 REDIS_DB = int(os.environ.get('COGS_REDIS_DB', config.get(SEC_REDIS, 'DB')))
 REDIS_PASSWORD = os.environ.get('COGS_REDIS_PASSWORD', config.get(SEC_REDIS, 'PASSWORD'))
-FILESTORAGE_PATH = os.path.realpath(os.environ.get('COGS_FILESTORAGE_PATH',
-                                                   config.get(SEC_FILESTORAGE, 'PATH')))
+FILESTORAGE_PATH = os.environ.get('COGS_FILESTORAGE_PATH', config.get(SEC_FILESTORAGE, 'PATH'))
+FILESTORAGE_PATH = os.path.realpath(FILESTORAGE_PATH)
+AUTHMOD_MOODLE_HOST = os.environ.get('COGS_AUTHMOD_MOODLE_HOST',
+                                     config.get(SEC_AUTHMOD_MOODLE, 'HOST'))
+AUTHMOD_MOODLE_SERVICE = os.environ.get('COGS_AUTHMOD_MOODLE_SERVICE',
+                                        config.get(SEC_AUTHMOD_MOODLE, 'SERVICE'))
+REPMOD_MOODLE_HOST = os.environ.get('COGS_REPMOD_MOODLE_HOST',
+                                    config.get(SEC_REPMOD_MOODLE, 'HOST'))
+REPMOD_MOODLE_SERVICE = os.environ.get('COGS_REPMOD_MOODLE_SERVICE',
+                                       config.get(SEC_REPMOD_MOODLE, 'SERVICE'))
+REPMOD_MOODLE_USERNAME = os.environ.get('COGS_REPMOD_MOODLE_USERNAME',
+                                        config.get(SEC_REPMOD_MOODLE, 'USERNAME'))
+REPMOD_MOODLE_PASSWORD = os.environ.get('COGS_REPMOD_MOODLE_PASSWORD',
+                                        config.get(SEC_REPMOD_MOODLE, 'PASSWORD'))
+
 SCRIPT_PATH = os.environ.get('COGS_SCRIPT_PATH', DEFAULT_SCRIPT_PATH)
 ENV_LOCAL_TMP_PATH = os.environ.get('COGS_ENV_LOCAL_TMP_PATH', DEFAULT_ENV_LOCAL_TMP_PATH)
 ENV_LOCAL_SANDBOX = os.environ.get('COGS_ENV_LOCAL_SANDBOX', DEFAULT_ENV_LOCAL_SANDBOX)
 TESTER_SCRIPT_USER = os.environ.get('COGS_TESTER_SCRIPT_USER', DEFAULT_TESTER_SCRIPT_USER)
 TESTER_SCRIPT_GROUP = os.environ.get('COGS_TESTER_SCRIPT_GROUP', DEFAULT_TESTER_SCRIPT_GROUP)
-AUTHMOD_MOODLE_HOST = os.environ.get('COGS_AUTHMOD_MOODLE_HOST', DEFAULT_AUTHMOD_MOODLE_HOST)
-AUTHMOD_MOODLE_SERVICE = os.environ.get('COGS_AUTHMOD_MOODLE_SERVICE', DEFAULT_AUTHMOD_MOODLE_SERVICE)
-REPMOD_MOODLE_HOST = os.environ.get('COGS_REPMOD_MOODLE_HOST', DEFAULT_REPMOD_MOODLE_HOST)
-REPMOD_MOODLE_SERVICE = os.environ.get('COGS_REPMOD_MOODLE_SERVICE', DEFAULT_REPMOD_MOODLE_SERVICE)
-REPMOD_MOODLE_USERNAME = os.environ.get('COGS_REPMOD_MOODLE_USERNAME', DEFAULT_REPMOD_MOODLE_USERNAME)
-REPMOD_MOODLE_PASSWORD = os.environ.get('COGS_REPMOD_MOODLE_PASSWORD', DEFAULT_REPMOD_MOODLE_PASSWORD)
