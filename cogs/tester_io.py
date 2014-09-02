a689d6a72b6d107ce4c4eae8d3928a3cbc1f6b60
Andy Sayler <andy.sayler@gmail.com>
2014-09-01 23:10:28 -0600

    Added logging and path-based file selection (untested)

--- cogs/tester_io.py
+++ cogs/tester_io.py
@@ -10,6 +10,7 @@ import subprocess
 import mimetypes
 import shutil
 import copy
+import logging
 
 import config
 
@@ -23,43 +24,110 @@ KEY_SOLUTION = 'solution'
 KEY_SUBMISSION = 'submission'
 KEY_INPUT = 'input'
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.NullHandler())
+
 
 class Tester(tester.Tester):
 
     def test(self):
 
-        # Find Reference Submission
+        ## Find Reference Solution
         sol_fle = None
-        count = 0
-        for fle in self.env.tst_files:
-            key = fle['key']
-            if (key == KEY_SOLUTION):
-                sol_fle = fle
-                count += 1
+
+        # If user provided path_solution, use that
+        if self.tst['path_solution']:
+            sol_path = "{:s}/{:s}".format(self.env.wd_tst, self.tst['path_solution'])
+            sol_path = os.path.normpath(sol_path)
+            for fle in self.env.tst_files:
+                fle_path = os.path.normpath(fle['path'])
+                if fle_path == sol_path:
+                    sol_fle = fle
+                    break
+            if not sol_fle:
+                msg = "User specified 'path_solution', but file {:s} not found".format(sol_path)
+                logger.warning(self._format_msg(msg))
+
+        # Next look for any files with the solution key
         if not sol_fle:
-            raise Exception("Tester module requires a reference solution file")
-        if (count > 1):
-            raise Exception("Tester module only supports suppling one solution file")
+            count = 0
+            for fle in self.env.tst_files:
+                key = fle['key']
+                if (key == KEY_SOLUTION):
+                    sol_fle = fle
+                    count += 1
+            if (count > 1):
+                msg = "Module only supports single reference solution, but {:d} found".format(count)
+                logger.error(self._format_msg(msg))
+                raise Exception(msg)
 
-        # Find Test Submission
+        # Raise error if not found
+        if not sol_fle:
+            msg = "Module requires a reference solution, but none found"
+            logger.error(self._format_msg(msg))
+            raise Exception(msg)
+
+        ## Find Test Submission
         sub_fle = None
-        count = 0
-        for fle in self.env.sub_files:
-            key = fle['key']
-            if (key == KEY_SUBMISSION):
-                sub_fle = fle
-                count += 1
-        if not sub_fle:
-            raise Exception("Tester module requires a submission file")
-        if (count > 1):
-            raise Exception("Tester module only supports suppling one submission file")
 
-        # Find Input Files
+        # If user provided path_submission, use that
+        if self.tst['path_submission']:
+            sub_path = "{:s}/{:s}".format(self.env.wd_tst, self.tst['path_submission'])
+            sub_path = os.path.normpath(sub_path)
+            for fle in self.env.sub_files:
+                fle_path = os.path.normpath(fle['path'])
+                if fle_path == sub_path:
+                    sub_fle = fle
+                    break
+            if not sub_fle:
+                msg = "User specified 'path_submission', but file {:s} not found".format(sub_path)
+                logger.warning(self._format_msg(msg))
+
+        # Next look for any files with the submission key
+        if not sub_fle:
+            count = 0
+            for fle in self.env.sub_files:
+                key = fle['key']
+                if (key == KEY_SUBMISSION):
+                    sub_fle = fle
+                    count += 1
+            if (count > 1):
+                msg = "Module only supports single submission, but {:d} found".format(count)
+                logger.error(self._format_msg(msg))
+                raise Exception(msg)
+
+        # Raise error if not found
+        if not sub_fle:
+            msg = "Module requires a submission, but none found"
+            logger.error(self._format_msg(msg))
+            raise Exception(msg)
+
+        ## Find Input Files
         input_fles = []
-        for fle in self.env.tst_files:
-            key = fle['key']
-            if (key == KEY_INPUT):
-                input_fles.append(fle)
+
+        # If user provided prefix_input, use that
+        if self.tst['prefix_input']:
+            input_prefix = "{:s}/{:s}".format(self.env.wd_tst, self.tst['prefix_input'])
+            input_prefix = os.path.normpath(input_prefix)
+            for fle in self.env.tst_files:
+                fle_path = os.path.normpath(fle['path'])
+                if fle_path.startswith(input_prefix):
+                    input_fles.append(fle)
+                    break
+            if not input_fles:
+                msg = ("User specified 'prefix_input', " +
+                       "but no files starting with {:s} were found".format(input_prefix))
+                logger.warning(self._format_msg(msg))
+
+        # Next look for any files with the input key
+        if not input_fles:
+            for fle in self.env.tst_files:
+                key = fle['key']
+                if (key == KEY_INPUT):
+                    input_fles.append(fle)
+
+        # Add None if no input files found
         if not input_fles:
             input_fles.append(None)
 
@@ -77,11 +145,11 @@ class Tester(tester.Tester):
 
         # Change WD
         owd = os.getcwd()
-        try:
-            os.chdir(self.env.wd)
-        except:
-            raise
+        msg = "Changing to directory '{:s}'".format(self.env.wd)
+        logger.debug(self._format_msg(msg))
+        os.chdir(self.env.wd)
 
+        # Run Test Inputs
         ret_val = 0
         pts = 0
         output = ""
@@ -98,6 +166,8 @@ class Tester(tester.Tester):
             else:
                 input_file = None
             cmd = sudo_cmd + sandbox_cmd + sol_cmd
+            msg = "Preparing to run '{:s}'".format(cmd)
+            logger.info(self._format_msg(msg))
             ret = 1
             stdout = ""
             stderr = ""
@@ -127,6 +197,8 @@ class Tester(tester.Tester):
             else:
                 input_file = None
             cmd = sudo_cmd + sandbox_cmd + sub_cmd
+            msg = "Preparing to run '{:s}'".format(cmd)
+            logger.info(self._format_msg(msg))
             ret = 1
             stdout = ""
             stderr = ""
@@ -156,13 +228,16 @@ class Tester(tester.Tester):
                 output += "   +0 pts\n"
 
         # Change Back to OWD
-        try:
-            os.chdir(owd)
-        except:
-            raise
+        msg = "Changing back to directory '{:s}'".format(owd)
+        logger.debug(self._format_msg(msg))
+        os.chdir(owd)
 
         # Calculate Score
         score = (pts / float(len(input_fles))) * float(self.tst['maxscore'])
+
+        # Log Results
+        msg = "retval='{:d}', score='{:.2f}', output='{:s}'".format(ret_val, score, output)
+        logger.info(self._format_msg(msg))
 
         # Return
         return ret_val, score, output
