8ba4e2c2e8abb2d12b3a4dcf2226eba10d03d0e1
Andy Sayler <andy.sayler@gmail.com>
2014-10-10 23:24:28 -0600

    Modifed testrun.py to handle builders (working to implement #3)

--- cogs/testrun.py
+++ cogs/testrun.py
@@ -8,6 +8,8 @@ import traceback
 import time
 
 import env_local
+import builder_make
+import builder_cmd
 import tester_script
 import tester_io
 
@@ -16,21 +18,61 @@ import auth
 
 def test(asn, sub, tst, run):
 
+    # Init
+    run['status'] = 'initializing_test'
+    retcode = 0
+    score = 0
+    env = None
+
     # Setup Env
-    try:
-        env_type = asn['env']
-        if env_type == 'local':
-            env = env_local.Env(asn, sub, tst, run)
-        else:
-            raise Exception("Unknown env type {:s}".format(env_type))
-    except Exception as e:
-        retcode = -1
-        score = 0
-        output = traceback.format_exc()
-        status = "complete-exception-env"
-    else:
-        # Setup Tester
+    if (retcode == 0):
         try:
+            run['status'] = 'initializing_env'
+            env_type = asn['env']
+            if env_type == 'local':
+                env = env_local.Env(asn, sub, tst, run)
+            else:
+                raise Exception("Unknown env type {:s}".format(env_type))
+        except Exception as e:
+            retcode = -1
+            score = 0
+            output = traceback.format_exc()
+            status = "complete-exception-env"
+
+    # Setup Builder
+    if (retcode == 0):
+        try:
+            run['status'] = 'initializing_build'
+            builder_type = tst['builder']
+            if builder_type == '':
+                builder = None
+            elif builder_type == 'make':
+                builder = builder_make.Builder(env, tst, run)
+            elif builder_type == 'cmd':
+                builder = builder_cmd.Builder(env, tst, run)
+            else:
+                raise Exception("Unknown builder type {:s}".format(builder_type))
+        except Exception as e:
+            retcode = -1
+            score = 0
+            output = traceback.format_exc()
+            status = "complete-exception-builder_setup"
+
+    # Run Build (If Necessary)
+    if (retcode == 0) and builder:
+        try:
+            run['status'] = 'building'
+            retcode, output = builder.build()
+        except Exception as e:
+            retcode = -1
+            score = 0
+            output = traceback.format_exc()
+            status = 'complete-exception-builder_build'
+
+    # Setup Tester
+    if (retcode == 0):
+        try:
+            run['status'] = 'initializing_run'
             tester_type = tst['tester']
             if tester_type == 'script':
                 tester = tester_script.Tester(env, tst, run)
@@ -42,55 +84,63 @@ def test(asn, sub, tst, run):
             retcode = -1
             score = 0
             output = traceback.format_exc()
-            status = "complete-exception-tst"
-        else:
-            # Run Test
-            try:
-                run['status'] = 'running'
-                retcode, score, output = tester.test()
-            except Exception as e:
-                retcode = -1
-                score = 0
-                output = traceback.format_exc()
-                status = 'complete-exception-run'
-            else:
-                if retcode == 0:
-                    status = 'complete'
-                else:
-                    status = 'complete-error'
-                retcode = str(retcode)
-                score = str(score)
-                output = str(output)
+            status = "complete-exception-tester_setup"
 
-                # Report Results
-                a = auth.Auth()
-                user = a.get_user(sub['owner'])
-                grade = score
-                comments = "COG Grading Report\n"
-                comments += "{:s}\n".format(time.strftime("%a %d %b %Y %H:%M:%S %Z"))
-                comments += "Assignment: {:s}\n".format(repr(asn))
-                comments += "Test: {:s}\n".format(repr(tst))
-                comments += "Submission: {:s}\n".format(repr(sub))
-                comments += "Run: {:s}\n".format(repr(run))
-                comments += "Run Score: {:s} of {:s}\n".format(score, tst['maxscore'])
-                comments += "Run Return: {:s}\n".format(retcode)
-                comments += "Run Status: {:s}\n".format(status)
-                comments += "Run Output:\n"
-                comments += output
-                for rpt in tst.get_reporters():
-                    try:
-                        rpt.file_report(run, user, grade, comments)
-                    except Exception as e:
-                        output += "\nWARNING: Reporting Failed: {:s}".format(e)
-                        status = "complete-exception-reporter"
-        # Cleanup
+    # Run Test
+    if (retcode == 0) and tester:
+        try:
+            run['status'] = 'running'
+            retcode, score, output = tester.test()
+        except Exception as e:
+            retcode = -1
+            score = 0
+            output = traceback.format_exc()
+            status = 'complete-exception-run'
+        else:
+            if retcode == 0:
+                status = 'complete'
+            else:
+                status = 'complete-error'
+
+    # Normalize Results
+    retcode = str(retcode)
+    score = str(score)
+    output = str(output)
+
+    # Report Results
+    reporters = tst.get_reporters()
+    if reporters:
+        run['status'] = 'reporting'
+        a = auth.Auth()
+        user = a.get_user(sub['owner'])
+        grade = score
+        comments = "COG Grading Report\n"
+        comments += "{:s}\n".format(time.strftime("%a %d %b %Y %H:%M:%S %Z"))
+        comments += "Assignment: {:s}\n".format(repr(asn))
+        comments += "Test: {:s}\n".format(repr(tst))
+        comments += "Submission: {:s}\n".format(repr(sub))
+        comments += "Run: {:s}\n".format(repr(run))
+        comments += "Run Score: {:s} of {:s}\n".format(score, tst['maxscore'])
+        comments += "Run Return: {:s}\n".format(retcode)
+        comments += "Run Status: {:s}\n".format(status)
+        comments += "Run Output:\n"
+        comments += output
+        for rpt in tst.get_reporters():
+            try:
+                rpt.file_report(run, user, grade, comments)
+            except Exception as e:
+                output += "\nWARNING: Reporting Failed: {:s}".format(e)
+                status = "complete-exception-reporter"
+
+    # Cleanup
+    run['status'] = 'cleaning_up'
+    if env:
         env.close()
 
-    finally:
-
-        # Update Run
-        # TODO Prevent update if run has been deleted
-        run['retcode'] = retcode
-        run['score'] = score
-        run['output'] = output
-        run['status'] = status # Must be set last to avoid race
+    # Update Run
+    run['status'] = 'saving'
+    # TODO Prevent update if run has been deleted
+    run['retcode'] = retcode
+    run['score'] = score
+    run['output'] = output
+    run['status'] = status # Must be set last to avoid race
