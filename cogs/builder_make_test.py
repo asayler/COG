8424f5cf088bc3f2eabb7b11e86caefd91672a01
Andy Sayler <andy.sayler@gmail.com>
2014-10-16 11:14:40 -0600

    Added working build test (resolves #3)

--- cogs/builder_make_test.py
+++ cogs/builder_make_test.py
@@ -154,6 +154,37 @@ class BuilderRunTestCase(builder_test.BuilderRunTestCase):
             self.print_fail(out)
             raise
 
+    def test_script_good(self):
+
+        # Add Tst Files
+        tst_files = [("grade_hello.py", 'script', None)]
+        fles = self.add_files(tst_files, self.tst)
+
+        # Add Sub Files
+        sub_files = [("hello_c/Makefile", 'makefile', None),
+                     ("hello_c/hello.c", 'source', None)]
+        fles = self.add_files(sub_files, self.sub)
+
+        # Run Test
+        run = self.run_test(self.sub)
+        out = run.get_dict()
+
+        # Cleanup
+        run.delete()
+        for fle in fles:
+            fle.delete()
+
+        # Check Output
+        try:
+            self.assertEqual(out['status'], "complete")
+            self.assertEqual(int(out['retcode']), 0)
+            self.assertTrue(out['output'])
+            self.assertIn("Score ---------------> 10" ,out['output'])
+            self.assertEqual(float(out['score']), 10)
+        except AssertionError:
+            self.print_fail(out)
+            raise
+
 # Main
 if __name__ == '__main__':
     unittest.main()
