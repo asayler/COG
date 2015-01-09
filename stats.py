8d3190e0abd4bf2fe95e7baee45092a6dfd270f4
Andy Sayler <andy.sayler@gmail.com>
2015-01-08 20:59:11 -0700

    Split out tests

--- stats.py
+++ stats.py
@@ -16,52 +16,42 @@ def assignment_stats(asn_uuid):
 
     asn = srv.get_assignment(asn_uuid)
 
-    submitter_subs = {}
-    submitter_runs = {}
+    owners_by_tst = {tst_uuid:{} for tst_uuid in asn.list_tests()}
 
     for sub_uuid in asn.list_submissions():
 
         sub = srv.get_submission(sub_uuid)
-        user_uuid = sub['owner']
-
-        if user_uuid in submitter_subs:
-            submitter_subs[user_uuid].append(sub)
-        else:
-            submitter_subs[user_uuid] = [sub]
+        owner_uuid = sub['owner']
 
         for run_uuid in sub.list_runs():
 
             run = srv.get_run(run_uuid)
+            tst_uuid = run['test']
 
-            if user_uuid in submitter_runs:
-                submitter_runs[user_uuid].append(run)
+            if owner_uuid in owners_by_tst[tst_uuid]:
+                owners_by_tst[tst_uuid][owner_uuid].append(run)
             else:
-                submitter_runs[user_uuid] = [run]
+                owners_by_tst[tst_uuid][owner_uuid] = [run]
 
-    sub_cnts = [len(subs) for subs in submitter_subs.values()]
-    sub_cnt_avgs = averages(sub_cnts)
+    for tst_uuid in owners_by_tst:
 
-    run_cnts = [len(runs) for runs in submitter_runs.values()]
-    run_cnt_avgs = averages(run_cnts)
+        tst = srv.get_test(tst_uuid)
+        runs_by_owner = owners_by_tst[tst_uuid]
 
-    print("")
-    print(asn['name'])
+        run_cnts = [len(runs) for runs in runs_by_owner.values()]
+        run_cnt_avgs = averages(run_cnts)
 
-    print("sub_submitters = {:d}".format(len(submitter_subs)))
-    print("sub_cnt_tot = {:d}".format(sum(sub_cnts)))
-    print("sub_cnt_max = {:d}".format(max(sub_cnts)))
-    print("sub_cnt_min = {:d}".format(min(sub_cnts)))
-    print("sub_cnt_mean = {:.2f}".format(sub_cnt_avgs[0])) 
-    print("sub_cnt_medn = {:d}".format(sub_cnt_avgs[1]))
-    print("sub_cnt_mode = {}".format(sub_cnt_avgs[2]))
 
-    print("run_submitters = {:d}".format(len(submitter_runs)))
-    print("run_cnt_tot = {:d}".format(sum(run_cnts)))
-    print("run_cnt_max = {:d}".format(max(run_cnts)))
-    print("run_cnt_min = {:d}".format(min(run_cnts)))
-    print("run_cnt_mean = {:.2f}".format(run_cnt_avgs[0])) 
-    print("run_cnt_medn = {:d}".format(run_cnt_avgs[1]))
-    print("run_cnt_mode = {}".format(run_cnt_avgs[2]))
+        print("")
+        print("{:s} ({:s})".format(asn['name'], asn_uuid))
+        print("{:s} ({:s})".format(tst['name'], tst_uuid))
+        print("submitters = {:d}".format(len(runs_by_owner)))
+        print("run_cnt_tot = {:d}".format(sum(run_cnts)))
+        print("run_cnt_max = {:d}".format(max(run_cnts)))
+        print("run_cnt_min = {:d}".format(min(run_cnts)))
+        print("run_cnt_mean = {:.2f}".format(run_cnt_avgs[0])) 
+        print("run_cnt_medn = {:d}".format(run_cnt_avgs[1]))
+        print("run_cnt_mode = {}".format(run_cnt_avgs[2]))
 
 def averages(l):
 
