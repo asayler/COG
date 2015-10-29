50ca32534d350c3f560e5e241e7067cfa30bfb70
Andy Sayler <andy.sayler@gmail.com>
2015-10-28 21:59:31 -0600

    Finished parse_permissions implmentation

--- parse_permissions.py
+++ parse_permissions.py
@@ -1,7 +1,22 @@
 import json
 import os.path
+import uuid
+import logging
 
 import cogs.auth
+
+# Constants
+
+GROUP_ALIAS_ANY = ["any", "all", "everyone"]
+GROUP_ALIAS_ADMIN = ["admin", "admins", "administrator", "administrators"]
+
+# Logger
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.NullHandler())
+
+# Functions
 
 def ep_norm(ep):
 
@@ -16,29 +31,74 @@ def ep_join(*args):
         ep += ep_norm(arg)
     return ep_norm(ep)
 
+def group_norm(group_name):
+
+    group_name = group_name.lower()
+
+    # Any case
+    if group_name in GROUP_ALIAS_ANY:
+        return cogs.auth.SPECIAL_GROUP_ANY
+
+    # Admin case
+    if group_name in GROUP_ALIAS_ADMIN:
+        return cogs.auth.SPECIAL_GROUP_ADMIN
+
+    # Other case
+    a = cogs.auth.Auth()
+    group_objs = [a.get_group(uid) for uid in a.list_groups()]
+    group_names = {obj["name"].lower(): str(obj.uuid) for obj in group_objs}
+
+    if group_name in group_names:
+        group_uuid = group_names[group_name]
+    else:
+        group_obj = a.create_group({"name": group_name})
+        group_uuid = str(group_obj.uuid)
+
+    return group_uuid
+
 def parse_file(path, ep_base=None):
 
+    # Clean Args
     path = os.path.abspath(path)
     if ep_base:
         ep_base = os.path.normpath(ep_base)
 
-    j = None
+    # Read file
+    js = None
     with open(path, 'r') as f:
-        j = json.load(f)
+        js = json.load(f)
 
-    groups = j["groups"]
-
+    # Parse JSON
+    out = []
+    groups = js["groups"]
     for group, perms in groups.iteritems():
+        group = group_norm(group)
         for perm in perms:
-
             if ep_base:
                 ep = ep_join(ep_base, perm["ep"])
             else:
                 ep = ep_norm(perm["ep"])
-
             method = perm["method"]
-            group = group
+            tup = (method, ep, group)
+            out.append(tup)
 
-            print(ep)
-            print(method)
-            print(group)
+    # Return
+    return out
+
+def set_perms(perms):
+
+    a = cogs.auth.Auth()
+    cnt = 0
+    for tup in perms:
+        method, ep, group = tup
+        ret = a.add_allowed_groups(method, ep, [group])
+        logger.info("Allow group {} for {} on {} -> {}".format(group, method, ep, ret))
+        cnt += ret
+    return cnt
+
+def set_perms_from_file(path, ep_base=None):
+
+    perms = parse_file(path, ep_base)
+    cnt = set_perms(perms)
+    logger.info("Added {} permissions from {}".format(cnt, path))
+    return cnt
