43482a4fdf1e4d2cd50ff5160e293828a2f0605a
Andy Sayler <andy.sayler@gmail.com>
2016-06-01 11:29:40 -0600

    Added orphaned reporters cleanup

--- util/maintaince.py
+++ util/maintaince.py
@@ -27,6 +27,22 @@ def cleanup_orphaned_files(srv):
 
     return deleted
 
+def cleanup_orphaned_reporters(srv):
+
+    attached = list_attached_reporters(srv)
+    reporters = srv.list_reporters()
+    orphans = reporters - attached
+    deleted = set()
+
+    for rpt_uid in orphans:
+        rpt = srv.get_reporter(rpt_uid)
+        mod_time = float(rpt.get_dict()['modified_time'])
+        if (time.time() - mod_time) > ORPHAN_AGE:
+            rpt.delete()
+            deleted.add(rpt_uid)
+
+    return deleted
+
 def list_attached_files(srv):
 
     attached = set()
@@ -47,9 +63,25 @@ def list_attached_files(srv):
 
     return attached
 
+def list_attached_reporters(srv):
+
+    attached = set()
+
+    # From Tests
+    tst_uids = srv.list_tests()
+    for tst_uid in tst_uids:
+        tst = srv.get_test(tst_uid)
+        rpt_uids = tst.list_reporters()
+        attached.update(rpt_uids)
+
+    return attached
+
 if __name__ == "__main__":
 
     srv = cogs.structs.Server()
-    
+
     orphans = cleanup_orphaned_files(srv)
     print("Cleaned up {} orphaned files".format(len(orphans)))
+
+    orphans = cleanup_orphaned_reporters(srv)
+    print("Cleaned up {} orphaned reporters".format(len(orphans)))
